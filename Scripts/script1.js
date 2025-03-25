fe4c2ea77be7818a3f29e97393fc7b6b10255078
Vinicius Gabriel de Souza Egidio <49576653894@>
2025-03-25 15:14:25 -0300

    25/03/2025 15:14

--- Scripts/script1.js
+++ Scripts/script1.js
@@ -1,15 +1,4 @@
 let numero1 = Number(prompt("Digite o primeiro número:"))
 let numero2 = Number(prompt("Digite o segundo número:"))
 let diferenca = numero1 - numero2
-if (numero1 > numero2) {
-    document.write(`A diferença entre ${numero1} e ${numero2} é igual a ${diferenca}`)
-}
-else {
-    while (numero2 > numero1) {
-        alert("O resultado será negativo. Digite o maior número primeiro")
-        numero1 = Number(prompt("Digite o promeiro número:"))
-        numero2 = Number(prompt("Digite o segundo número:"))
-        diferenca = numero1 - numero2
-    }
-    document.write(`A diferença entre ${numero1} e ${numero2} é ${diferenca}`)
-}
+document.write(`A diferença entre ${numero1} e ${numero2} é igual a ${diferenca}`)
--- Scripts/script13.js
+++ Scripts/script13.js
@@ -1,36 +1,27 @@
-let operacao = prompt("Escolha uma operação pelo símbolo: /, *, -, +")
-if (operacao == "/") {
-    let numero1 = parseFloat(prompt("Digite um número:"))
-    let numero2 = parseFloat(prompt("Digite um número:"))
-    const calculadora = (numero1, numero2) => {
-        return numero1 / numero2
+const calculadora = (simbolo, num1, num2) => {
+    if (simbolo === "/") {
+        const divisao = num1 / num2 
+        if (num2 === 0) {
+            return `O divisor não pode ser 0`
+        }
+        return `Resultado da divisão: ${divisao.toFixed(2)}`
     }
-    document.write(`Resultado da divisão: ${calculadora(numero1, numero2)}`) 
-}
-else if (operacao == "*") {
-    let numero1 = parseFloat(prompt("Digite um número:"))
-    let numero2 = parseFloat(prompt("Digite um número:"))
-    const calculadora = (numero1, numero2) => {
-        return numero1 * numero2
+    else if (simbolo === "*") {
+        const multiplicacao = num1 * num2  
+        return `Resultado da multiplicação: ${multiplicacao.toFixed(2)}`
     }
-    document.write(`Resultado da multplicação: ${calculadora(numero1, numero2)}`)
-}
-else if (operacao == "-") {
-    let numero1 = parseFloat(prompt("Digite um número:"))
-    let numero2 = parseFloat(prompt("Digite um número:"))
-    const calculadora = (numero1, numero2) => {
-        return numero1 - numero2
+    else if (simbolo === "-") {
+        const subtracao = num1 - num2  
+        return `Resultado de subtração: ${subtracao.toFixed(2)}`
     }
-    document.write(`Resultado da subtração: ${calculadora(numero1, numero2)}`)
-}
-else if (operacao == "+") {
-    let numero1 = parseFloat(prompt("Digite um número:"))
-    let numero2 = parseFloat(prompt("Digite um número:"))
-    const calculadora = (numero1, numero2) => {
-        return numero1 + numero2
+    else if (simbolo === "+") {
+        const adicao = num1 + num2  
+        return `Resultado da adição: ${adicao.toFixed(2)}`
     }
-    document.write(`Resultado da soma: ${calculadora(numero1, numero2)}`)
 }
-else {
-    document.write("Operação inválida. Reinicie a página")
-}
+
+const simbolo = prompt("Digite o símbolo para a operação: /, *, -, +")
+const num1 = parseFloat(prompt("Digite um número:"))
+const num2 = parseFloat(prompt("Digite um número:"))
+const resultado = calculadora(simbolo, num1, num2)  
+document.write(resultado)
