e335524d432759f00c7d9452598658e195ac6a24
Vinicius Gabriel de Souza Egidio <49576653894@>
2025-03-25 16:17:51 -0300

    25/03/2025 16:17

--- Scripts/script25.js
+++ Scripts/script25.js
@@ -1,5 +1,5 @@
 let valor = parseFloat(prompt("Digite o valor do financiamento:"))
-let taxa = parseFloat(prompt("Digite o valor da taxa em decimal:"))
-let parcelas = parseInt(prompt("Digite a qauntidade de parcelas:"))
+let taxa = parseFloat(prompt("Digite o valor da taxa em decimal. Exemplo: 0.50"))
+let parcelas = parseInt(prompt("Digite a quantidade de parcelas:"))
 let financiamento = (valor + (valor * taxa * parcelas)) / parcelas
 document.write(`Valor de cada parcela: R$ ${financiamento.toFixed(2)}`)
