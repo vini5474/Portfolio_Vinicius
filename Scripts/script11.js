c243bd3ee7e4d314a3cab71ecb537a2123db12a6
Vinicius Egidio <vini5474>
2025-03-25 21:21:43 -0300

    25/03/2025 21:21

--- Scripts/script11.js
+++ Scripts/script11.js
@@ -4,26 +4,34 @@ if (jogador1 == "pedra" && jogador2 == "tesoura") {
     document.write(`Jogador 1 venceu`)
 }
 else if (jogador2 == "pedra" && jogador1 == "tesoura") {
+    document.write(`Jogador 1 escolheu: ${jogador1} <br> Jogador 2 escolheu: ${jogador2} <br>`)
     document.write(`Jogador 2 venceu`)
 }
 else if (jogador1 == "papel" && jogador2 == "tesoura") {
+    document.write(`Jogador 1 escolheu: ${jogador1} <br> Jogador 2 escolheu: ${jogador2} <br>`)
     document.write(`Jogador 2 venceu`)
 }
 else if (jogador2 == "papel" && jogador1 == "tesoura") {
+    document.write(`Jogador 1 escolheu: ${jogador1} <br> Jogador 2 escolheu: ${jogador2} <br>`)
     document.write(`Jogador 1 venceu`)
 }
 else if (jogador1 == "papel" && jogador2 == "papel") {
+    document.write(`Jogador 1 escolheu: ${jogador1} <br> Jogador 2 escolheu: ${jogador2} <br>`)
     document.write(`Houve um empate`)
 }
 else if (jogador1 == "tesoura" && jogador2 == "tesoura") {
+    document.write(`Jogador 1 escolheu: ${jogador1} <br> Jogador 2 escolheu: ${jogador2} <br>`)
     document.write(`Houve um empate`)
 }
 else if (jogador1 == "pedra" && jogador2 == "pedra") {
+    document.write(`Jogador 1 escolheu: ${jogador1} <br> Jogador 2 escolheu: ${jogador2} <br>`)
     document.write(`Houve um empate`)
 }
 else if (jogador1 == "papel" && jogador2 == "pedra") {
+    document.write(`Jogador 1 escolheu: ${jogador1} <br> Jogador 2 escolheu: ${jogador2} <br>`)
     document.write(`Jogador 1 venceu`)
 }
 else if (jogador1 == "pedra" && jogador2 == "papel") {
+    document.write(`Jogador 1 escolheu: ${jogador1} <br> Jogador 2 escolheu: ${jogador2} <br>`)
     document.write(`Jogador 2 venceu`)
 }
