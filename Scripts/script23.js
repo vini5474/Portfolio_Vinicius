0d0fed9e5168f3c7f225b06632c1904a1f37e01a
Vinicius Egidio <vini5474>
2025-03-30 16:45:10 -0300

    30/03/2025 16:45

--- Scripts/script23.js
+++ Scripts/script23.js
@@ -1,22 +1,26 @@
 const inss = (salario) => {
     if (salario <= 1580) {
-        const insscalculado = (salario * 0.075) 
-        return `Calculo INSS: R$ ${insscalculado.toFixed(2)}.<br>`
+        const insscalculado = salario * 0.075;
+        return `Cálculo INSS: R$ ${insscalculado.toFixed(2)}.<br>`
     }
     else if (salario <= 2793.88) {
-        const insscalculado = (salario * 0.09) - 22.77  
-        return `Calculo INSS: R$ ${insscalculado.toFixed(2)}.<br>`
+        const insscalculado = salario * 0.09 - 22.77;
+        return `Cálculo INSS: R$ ${insscalculado.toFixed(2)}.<br>`
     }
     else if (salario <= 4190.84) {
-        const insscalculado = (salario * 0.12) - 106.59  
-        return `Calculo INSS: R$ ${insscalculado.toFixed(2)}.<br>`
+        const insscalculado = salario * 0.12 - 106.59;
+        return `Cálculo INSS: R$ ${insscalculado.toFixed(2)}.<br>`
     }
     else if (salario <= 8157.41) {
-        const insscalculado = (salario * 0.14) - 190.40  
-        return `Calculo INSS: R$ ${insscalculado.toFixed(2)}.<br>`
+        const insscalculado = salario * 0.14 - 190.40;
+        return `Cálculo INSS: R$ ${insscalculado.toFixed(2)}.<br>`
+    }
+    else {
+        const insscalculado = 8157.41 * 0.14 - 190.40;
+        return `Cálculo INSS (teto): R$ ${insscalculado.toFixed(2)}.<br>`
     }
 }
 
 const salario = parseFloat(prompt("Digite o seu salário:"))
-const resultado = inss(salario)  
+const resultado = inss(salario) 
 document.write(resultado)
